787cb6a24aa23c2f26dc7a327c73bfafe7c1cd66
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-08-27 02:19:27 -0700

    Update imports.lua

--- client/imports.lua
+++ client/imports.lua
@@ -1,1 +1,30 @@
-Feather = exports['feather-core'].initiate()
+Feather = {}
+Feather.RPC = {
+  CallAsync = function(name, params, source, timeoutMs)
+    return exports['feather-core']:CallRPCAsync(name, params, source, timeoutMs)
+  end
+}
+Feather.Locale = {
+  register = function(locale, translations)
+    return exports['feather-core']:RegisterLocale(locale, translations)
+  end,
+  translate = function(source, key, ...)
+    local result = exports['feather-core']:TranslateLocale(source, key, ...)
+    return type(result) == 'table' and result.ok == true and result.value
+      or ('Translation [%s] is unavailable'):format(tostring(key))
+  end
+}
+
+Feather.Notify = {}
+Feather.Notify.RightNotify = function(message, duration)
+  local result = exports['feather-core']:ShowNotification({
+    style = 'right',
+    message = message,
+    duration = duration
+  })
+  if type(result) ~= 'table' or result.ok ~= true then
+    print(('[feather-inventory] client notification failed code=%s'):format(
+      tostring(type(result) == 'table' and result.code or 'invalid_result')))
+  end
+  return result
+end
